ace689bb7852755a6a8c2dd290338bb3ef891c74
Zacharias Tyllström <zache21@hotmail.se>
2020-02-18 15:34:47 +0100

    Started working on main in GomokuMain.

--- src/lab4/GomokuMain.java
+++ src/lab4/GomokuMain.java
@@ -1,10 +1,25 @@
 package lab4;
 
+import lab4.client.GomokuClient;
+import lab4.data.GomokuGameState;
+import lab4.gui.GomokuGUI;
+
 /**
  *
  */
 public class GomokuMain {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int portNumber = 4000;
+        if (args.length <= 1) {
+            portNumber = Integer.parseInt(args[0]);
+        } else {
+            System.out.println("Too many arguments. Shutting down!");
+            System.exit(0);
+        }
+
+        System.out.println("Opening game to port " + portNumber);
+        GomokuClient gc = new GomokuClient(portNumber);
+        GomokuGameState gs = new GomokuGameState(gc);
+        GomokuGUI gg = new GomokuGUI(gs, gc);
     }
 }
