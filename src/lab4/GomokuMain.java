b4207259c0634fbd7ea403b5e6fefbec57620ff0
Zacharias Tyllström <zache21@hotmail.se>
2020-02-18 15:49:52 +0100

    Added portNumber read and object instantiation and some error handling.

--- src/lab4/GomokuMain.java
+++ src/lab4/GomokuMain.java
@@ -8,12 +8,19 @@ import lab4.gui.GomokuGUI;
  *
  */
 public class GomokuMain {
+    private static int portNumber;
+    private static final int maxArgs = 1;
+
+
     public static void main(String[] args) {
-        int portNumber = 4000;
-        if (args.length <= 1) {
+        portNumber = 4000;
+        if (args.length == maxArgs) {
             portNumber = Integer.parseInt(args[0]);
+        } else if (args.length = 0) {
+            portNumber =
         } else {
-            System.out.println("Too many arguments. Shutting down!");
+            System.out.println("Incorrect number of arguments. Expected: " + maxArgs);
+            System.out.println("Shutting down!");
             System.exit(0);
         }
 
