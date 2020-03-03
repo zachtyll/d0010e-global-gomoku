a074f956cbd51607343acfe2c5fb0284afcc9566
Zacharias Tyllström <zache21@hotmail.se>
2020-03-03 11:29:26 +0100

    Moving things around.

--- src/lab4/gui/GomokuGUI.java
+++ src/lab4/gui/GomokuGUI.java
@@ -20,12 +20,13 @@ public class GomokuGUI implements Observer{
 	private GomokuGameState gamestate;
 
 	private JFrame mainFrame;
-	private JLabel headerLabel;
-	private JLabel statusLabel;
 	private JLabel messageLabel;
 	private JPanel buttonPanel;
 	private JPanel boardPanel;
-	private JPanel controlPanel;
+
+	private final int width;
+	private final int height;
+	private GamePanel gameGridPanel;
 
 	/**
 	 * The constructor
@@ -39,10 +40,15 @@ public class GomokuGUI implements Observer{
 		client.addObserver(this);
 		gamestate.addObserver(this);
 
+		gameGridPanel = new GamePanel(g.getGameGrid());
+		width = gamestate.getGameGrid().getSize() * gameGridPanel.UNIT_SIZE;
+		height = gamestate.getGameGrid().getSize() * gameGridPanel.UNIT_SIZE;
+
 		// Setup main frame.
 		mainFrame = new JFrame("Global Gomoku");
-		mainFrame.setSize(1000,1000);
-		mainFrame.setLayout(new GridLayout(3, 1));
+		mainFrame.setSize(450, 450);
+		mainFrame.setLayout(new GridLayout(3, 1, 10, 10));
+
 
 		mainFrame.addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent windowEvent){
@@ -51,42 +57,33 @@ public class GomokuGUI implements Observer{
 			}
 		});
 
+		Container contentPanel = mainFrame.getContentPane();
+		GroupLayout groupLayout = new GroupLayout(contentPanel);
+		contentPanel.setLayout(groupLayout);
+		groupLayout.setAutoCreateGaps(true);
+		groupLayout.setAutoCreateContainerGaps(true);
+
 		// Setup labels.
-		headerLabel = new JLabel("", JLabel.CENTER);
-		statusLabel = new JLabel("", JLabel.CENTER);
-		statusLabel.setSize(350,100);
 		messageLabel = new JLabel("messageLabel", JLabel.CENTER);
+		messageLabel.setBorder(BorderFactory.createLineBorder(Color.black));
 
 		// Setup Board panel.
 		boardPanel = new JPanel();
 		boardPanel.setLayout(new FlowLayout());
+		boardPanel.setSize(width, height);
+		boardPanel.setBorder(BorderFactory.createLineBorder(Color.black));
 
 		// Setup buttons panel.
 		buttonPanel = new JPanel();
 		buttonPanel.setLayout(new FlowLayout());
+		boardPanel.setBorder(BorderFactory.createLineBorder(Color.black));
 
-		// Setup grouping panel
-		controlPanel = new JPanel();
-		controlPanel.setLayout(new BoxLayout(controlPanel, BoxLayout.Y_AXIS));
-
-		// Add components.
-//		mainFrame.add(gameGridPanel);
-		mainFrame.add(controlPanel);
-		mainFrame.add(headerLabel);
-		mainFrame.add(statusLabel);
-		mainFrame.setVisible(true);
-	}
-	
-	public void update(Observable arg0, Object arg1) {
 		// Setup Board
-		GamePanel gameGridPanel = new GamePanel(gamestate.getGameGrid());
-		int width = gamestate.getGameGrid().getSize() * gameGridPanel.UNIT_SIZE;
-		int height = gamestate.getGameGrid().getSize() * gameGridPanel.UNIT_SIZE;
 		gameGridPanel.setSize(width, height);
 
 		gameGridPanel.addMouseListener(new MouseAdapter() {
 			public void mouseClicked(MouseEvent e) {
-				statusLabel.setText("x: " + e.getX() + "y: " + e.getY());
+				messageLabel.setText("x: " + e.getX() + "y: " + e.getY());
 			}
 		});
 
@@ -96,7 +93,7 @@ public class GomokuGUI implements Observer{
 
 		connectButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				statusLabel.setText("connectButton clicked.");
+				messageLabel.setText("connectButton clicked.");
 			}
 		});
 		newGameButton.addActionListener(new ActionListener() {
@@ -106,19 +103,65 @@ public class GomokuGUI implements Observer{
 		});
 		disconnectButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				statusLabel.setText("disconnectButton clicked.");
+				messageLabel.setText("disconnectButton clicked.");
 			}
 		});
+
+		GroupLayout.SequentialGroup hGroup = groupLayout.createSequentialGroup();
+		GroupLayout.SequentialGroup vGroup = groupLayout.createSequentialGroup();
+
+		hGroup.addGroup(groupLayout.createParallelGroup().
+				addComponent(boardPanel).addComponent(buttonPanel).addComponent(messageLabel));
+		groupLayout.setHorizontalGroup(hGroup);
+
+		vGroup.addGroup(groupLayout.createParallelGroup(GroupLayout.Alignment.BASELINE).
+				addComponent(boardPanel));
+		vGroup.addGroup(groupLayout.createParallelGroup(GroupLayout.Alignment.BASELINE).
+				addComponent(buttonPanel));
+		vGroup.addGroup(groupLayout.createParallelGroup(GroupLayout.Alignment.BASELINE).
+				addComponent(messageLabel));
+		groupLayout.setVerticalGroup(vGroup);
+
+
+		mainFrame.setVisible(true);
+	}
+	
+	public void update(Observable arg0, Object arg1) {
+//		// Setup Board
+//		gameGridPanel.setSize(width, height);
+//
+//		gameGridPanel.addMouseListener(new MouseAdapter() {
+//			public void mouseClicked(MouseEvent e) {
+//				messageLabel.setText("x: " + e.getX() + "y: " + e.getY());
+//			}
+//		});
+//
+//		JButton connectButton = new JButton("connectButton");
+//		JButton newGameButton = new JButton("newGameButton");
+//		JButton disconnectButton = new JButton("disconnectButton");
+//
+//		connectButton.addActionListener(new ActionListener() {
+//			public void actionPerformed(ActionEvent e) {
+//				messageLabel.setText("connectButton clicked.");
+//			}
+//		});
+//		newGameButton.addActionListener(new ActionListener() {
+//			public void actionPerformed(ActionEvent e) {
+//				messageLabel.setText("newGameButton clicked.");
+//			}
+//		});
+//		disconnectButton.addActionListener(new ActionListener() {
+//			public void actionPerformed(ActionEvent e) {
+//				messageLabel.setText("disconnectButton clicked.");
+//			}
+//		});
 
 		boardPanel.add(gameGridPanel);
 
 		buttonPanel.add(connectButton);
 		buttonPanel.add(newGameButton);
 		buttonPanel.add(disconnectButton);
-
-		controlPanel.add(boardPanel);
-		controlPanel.add(buttonPanel);
-		controlPanel.add(messageLabel);
+		buttonPanel.add(messageLabel);
 
 		mainFrame.setVisible(true);
 
